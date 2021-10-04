31924dab4d5fad493554b6f02a843623a41b8ed3
Louis Chavez <67764701+PonguTracer@users.noreply.github.com>
2021-10-04 16:57:33 -0700

    Add files via upload

---
+++ access_python_connect.py
@@ -0,0 +1,9 @@
+import pyodbc
+
+conn = pyodbc.connect(r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
+            r'DBQ=C:\AutoShop\w_orders.accdb;')
+cursor = conn.cursor()
+cursor.execute('select * from Orders')
+
+for row in cursor.fetchall():
+    print(row)
